2ebf70b0fa7a75af8de5b999aa4c39117fa1a45d
paul <paul@pauls-MacBook-Pro.local>
2020-12-22 18:19:58 +0600

    container fix

--- components/ui/container.js
+++ components/ui/container.js
@@ -5,12 +5,12 @@ export default function Container({
 	style = {},
 }) {
 	const defaultClasses = fluid
-		? 'px-5 lg:px-8 xl:px-16 w-full mx-auto'
-		: 'container mx-auto px-4';
-	const fluidStyle = fluid ? { maxWidth: '1920px' } : style;
+		? 'px-4 lg:px-8 2xl:px-16 w-full max-w-rq-1920 mx-auto'
+		: 'container mx-auto px-4 lg:px-8';
+	// const fluidStyle = fluid ? { maxWidth: '1920px' } : style;
 
 	return (
-		<div className={`${defaultClasses} ${className}`} style={fluidStyle}>
+		<div className={`${defaultClasses} ${className}`} style={style}>
 			{children}
 		</div>
 	);
