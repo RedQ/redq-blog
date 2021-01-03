7c3aa64d86a430c5de77b72d82ddf8f43b7d3859
Neamat Khan Mim <neamatmim@gmail.com>
2021-01-03 12:54:37 +0600

    stop animations

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 import Sticky from 'react-stickynode';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import Meta from '../meta';
@@ -6,24 +6,24 @@ import Header from './header';
 import Footer from './footer/footer';
 
 export default function Layout({ children }) {
-	return (
-		<>
-			<Meta />
-			{/* End of default SEO */}
-			<ParallaxProvider>
-				<motion.main
-					initial="exit"
-					animate="enter"
-					exit="exit"
-					className="flex flex-col min-h-screen"
-				>
-					<Sticky top={0} innerZ={9999} activeClass="sticky-nav">
-						<Header />
-					</Sticky>
-					<div className="flex-grow">{children}</div>
-					<Footer />
-				</motion.main>
-			</ParallaxProvider>
-		</>
-	);
+  return (
+    <>
+      <Meta />
+      {/* End of default SEO */}
+      <ParallaxProvider>
+        <div
+          // initial="exit"
+          // animate="enter"
+          // exit="exit"
+          className="flex flex-col min-h-screen"
+        >
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav">
+            <Header />
+          </Sticky>
+          <div className="flex-grow">{children}</div>
+          <Footer />
+        </div>
+      </ParallaxProvider>
+    </>
+  );
 }
